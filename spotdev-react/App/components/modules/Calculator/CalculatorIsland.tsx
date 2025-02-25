5e3c8ee3ce33870f2d2683f5a634e16c18373513
Dennis <Mvajteseczyhy1>
2025-02-25 20:35:13 +0200

    add bg color

--- spotdev-react/App/components/modules/Calculator/CalculatorIsland.tsx
+++ spotdev-react/App/components/modules/Calculator/CalculatorIsland.tsx
@@ -2,6 +2,7 @@ import { CalculatorProps } from "./Calculator.type.tsx"
 import TextComponent from "../../TextComponent/TextComponent.tsx";
 import React from "react";
 import CalculatorCard from "../../CalculatorCard/index.tsx";
+import classes from "./Calculator.module.css"
 
 const CalculatorIsland = (props: CalculatorProps) => {    
     const { fieldValues } = props;
@@ -10,7 +11,7 @@ const CalculatorIsland = (props: CalculatorProps) => {
     
     
     return (
-        <div>
+        <div className={`${classes.colorbackground}`}>
             <TextComponent textTitle={textTitle} textSection={textSection} />
             <p>test</p>
             <CalculatorCard cardTitle={"test title"} firstNumber={5} secondNumber={5}></CalculatorCard>
